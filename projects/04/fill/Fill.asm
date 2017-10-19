978f2645ed150a90e36866e30363202172b61203
Ben Maraney <ben.maraney@bigpanda.io>
2017-10-19 17:39:11 +0300

    Project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,65 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(WAITPRESS)
+//if keyboard = 0  GOTO WAITPRESS
+  @KBD
+  D=M
+  @WAITPRESS
+  D;JEQ
+
+//i =8192
+  @8191
+  D=A
+  @i
+  M=D
+(DRAW)
+//screen[i]  = -1
+  @i
+  D=M
+  @SCREEN
+  A=A+D
+  M=-1
+// i--
+  @i
+  M=M-1
+//GOTO draw if i>0
+  D=M
+  @DRAW
+  D;JGE
+
+(WAITRELEASE)
+//if keyboard > 0 GOTO WAITRELEASE
+  @KBD
+  D=M
+  @WAITRELEASE
+  D;JGT
+
+//i =8192
+  @8191
+  D=A
+  @i
+  M=D
+(UNDRAW)
+//screen[i]  = -1
+  @i
+  D=M
+  @SCREEN
+  A=A+D
+  M=0
+// i--
+  @i
+  M=M-1
+//GOTO draw if i>0
+  D=M
+  @UNDRAW
+  D;JGE
+
+//GOTO WAITPRESS
+  @WAITPRESS
+  0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,33 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// i = 1
+  @i
+  M=0
+// sum = 0
+  @R2
+  M=0
+// LOOP:
+(LOOP)
+// If i >= R1 goto end BUG HERE
+  @i
+  D=M
+  @R1
+  D=M-D
+  @END
+  D;JLE
+// sum += R0
+  @R0
+  D=M
+  @R2
+  M=M+D
+// i++
+  @i
+  M=M+1
+// GOTO LOOP
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
+// END: goto end
